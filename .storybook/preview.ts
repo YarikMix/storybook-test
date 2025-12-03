b003dcef2654c233c560282407b58d25a60cbda8
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-12-03 20:53:01 +0300

    feat: ci cd

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,20 +1,2 @@
 import '@/styles/constants.scss';
 
-export const parameters = {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
-        },
-    },
-    backgrounds: {
-        default: 'dark',
-        values: [
-            {
-                name: 'dark',
-                value: '#0B1220'
-            }
-        ],
-    },
-}
